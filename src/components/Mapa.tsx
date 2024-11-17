1fa0979421dc8cd35b2d9b0ada02348754f46667
Yordis Monteserin <tcoscode@gmail.com>
2024-11-16 23:19:08 -0800

    fix icons

--- src/components/Mapa.tsx
+++ src/components/Mapa.tsx
@@ -1,5 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
 import "leaflet/dist/leaflet.css";
+
+const icon = new L.Icon({
+  iconUrl: "/leaflet/marker-icon.png", // Ruta correcta a tu archivo
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png", // Ruta correcta a tu archivo 2x
+  shadowUrl: "/leaflet/marker-shadow.png", // Ruta correcta al archivo de sombra
+});
 
 const Mapa = ({ lat, lon }: { lat: number; lon: number }) => {
   const position: [number, number] = [lat, lon];
@@ -19,7 +27,7 @@ const Mapa = ({ lat, lon }: { lat: number; lon: number }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={icon}>
         <Popup>This is the position</Popup>
       </Marker>
     </MapContainer>
